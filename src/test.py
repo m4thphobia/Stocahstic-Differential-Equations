8a8b0cef019521ac4311d77207382aefd95d4d9c
m4thphobia <m4thphobia@gmail.com>
2023-10-29 01:29:13 +0900

    feat:gitignored added

--- src/test.py
+++ src/test.py
@@ -2,12 +2,12 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 np.random.seed(42)
+
+
 # get folder path
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
 output_folder = os.path.join(parent_dir, "out")
-# make folder if not exist
-# if not os.path.exists(output_folder):
-#    os.makedirs(output_folder)
 
 print(output_folder)
+print(output_folder+f"\{os.path.basename(__file__).split('.')[0]}.png")
