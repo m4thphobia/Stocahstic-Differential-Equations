d3b8a48daa25f364d586f3c1ea7a18d20a1760ab
m4thphobia <m4thphobia@gmail.com>
2023-10-28 17:17:40 +0900

    feat:geoBM completed

--- geometricBMprocess.py
+++ geometricBMprocess.py
@@ -11,5 +11,19 @@ if not os.path.exists(output_folder):
 T, N = 1, 500
 dt = T/N
 t = np.arange(0, 1, dt)
-B = np.cumsum(np.sqrt(dt)*np.random.randn(N))
+M = 1000
+B = np.cumsum(np.sqrt(dt)*np.random.randn(M, N), axis=1)
 
+Y = np.exp(t)
+U = np.exp(np.tile(t, (M, 1))+0.5*B)
+Umean = np.mean(U, axis=0)
+
+plt.title(f"{os.path.basename(__file__).split('.')[0]}")
+for i in range(10):
+    plt.plot(t, U[i, :], linestyle='--', color="m", linewidth=0.5)
+plt.plot(t, Umean, color="r", label="mean")
+plt.plot(t, Y, color="b", label="trend")
+plt.xlabel("")
+plt.ylabel("")
+plt.legend()
+plt.savefig(output_folder+f"\{os.path.basename(__file__).split('.')[0]}.png")
